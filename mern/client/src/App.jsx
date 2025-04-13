c84e55dd2f311ab5fc2a88f598d6dff6b37cc891
klam <klam11504@gmail.com>
2025-04-13 05:16:00 -0700

    changed landing to landing.jsx

--- mern/client/src/App.jsx
+++ mern/client/src/App.jsx
@@ -3,7 +3,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import NotFound from './NotFound';
 import './styles/App.css'
 import './styles/Landing.css'
-import Landing from './signup-login/landing'
+import Landing from './signup-login/landing.jsx'
 import Login from './signup-login/Login'
 import Signup from './signup-login/Signup'
 import Home from './home/Home'
